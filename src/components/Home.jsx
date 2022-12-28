d40dc1938642843d7b075c9616bb6cd2d9fa74e0
elinoza <hillcakmak@gmail.com>
2022-12-28 14:02:32 +0300

    boiler plate

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+
+  
+  } from "react-bootstrap";
+
+
+  class Home extends Component {
+ 
+  
+    
+    render() {
+      return (
+      <div>
+    
+      </div> 
+      );
+    }
+  }
+  export default Home;
